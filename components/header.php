3fa9b071ccf90d3a61768c60e1a8a409aa785fa4
Yanis <yanisamandpro@gmail.com>
2025-10-15 10:26:49 +0200

    V1 avec correction grace au feedback

--- components/header.php
+++ components/header.php
@@ -1,23 +1,15 @@
-<?php
-/**
- * Header Component
- * Reusable navigation header
- */
 
-// Define base path for navigation
-$basePath = dirname($_SERVER['PHP_SELF']);
-$isInPages = strpos($basePath, '/pages') !== false;
-$baseUrl = $isInPages ? '..' : '.';
-?>
+<head>
 <link rel="stylesheet" href="assets/css/main.css">
+<head>
 <header class="main-header">
     <nav class="navbar">
         <div class="nav-container">
             <!-- Logo -->
             <div class="nav-logo">
-                <a href="<?php echo $baseUrl; ?>/index.php">
+                <a href="../index.php">
                     <img 
-                        src="<?php echo $baseUrl; ?>/assets/images/logo.png" 
+                        src="../assets/images/logo.png" 
                         alt="ShopList Logo" 
                         class="logo-image"
                     >
@@ -34,23 +26,23 @@ $baseUrl = $isInPages ? '..' : '.';
             <div class="nav-menu" id="navMenu">
                 <ul class="nav-list">
                     <li class="nav-item">
-                        <a href="<?php echo $baseUrl; ?>/index.php" class="nav-link">Accueil</a>
+                        <a href="../index.php" class="nav-link">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo $baseUrl; ?>/pages/store_list.php" class="nav-link">Listes Des Magasins</a>
+                        <a href="../pages/store_list.php" class="nav-link">Listes Des Magasins</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo $baseUrl; ?>/pages/about.php" class="nav-link">À propos</a>
+                        <a href="../pages/about.php" class="nav-link">À propos</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo $baseUrl; ?>/pages/contact.php" class="nav-link">Contact</a>
+                        <a href="../pages/contact.php" class="nav-link">Contact</a>
                     </li>
                 </ul>
 
                 <!-- Auth Buttons -->
                 <div class="nav-auth">
-                    <a href="<?php echo $baseUrl; ?>/pages/login.php" class="btn btn-outline">Connexion</a>
-                    <a href="<?php echo $baseUrl; ?>/pages/register.php" class="btn btn-primary">Inscription</a>
+                    <a href="../pages/login.php" class="btn btn-outline">Connexion</a>
+                    <a href="../pages/register.php" class="btn btn-primary">Inscription</a>
                 </div>
             </div>
         </div>
